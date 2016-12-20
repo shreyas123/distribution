0481d64a4701c6195d959045d6322cb39794ad44
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2016-12-21 00:07:01 +0100

    Write to a tmp zip file

--- lib/zion/sentinel.rb
+++ lib/zion/sentinel.rb
@@ -2,10 +2,11 @@ require 'zip'
 
 module Zion
   class Sentinel < Zion::Source
-    FILE_NAME = 'sentinels.zip'
+    FILE_NAME = '/tmp/sentinels.zip'
 
     def get_data
       data = hack_matrix({ source: 'sentinels' })
+      File.write(FILE_NAME, data)
     end
   end
 end
--- spec/zion/sentinel_spec.rb
+++ spec/zion/sentinel_spec.rb
@@ -8,5 +8,16 @@ describe Zion::Sentinel do
       expect(obj).to receive(:hack_matrix).with({source: 'sentinels'}).and_return ''
       obj.get_data
     end
+
+    context 'when data is not expected but allowed' do
+      before do
+        allow(obj).to receive(:hack_matrix).with({source: 'sentinels'}).and_return 'tmpdata'
+      end
+
+      it 'writes the data to the tmp zip file' do
+        expect(File).to receive(:write).with(described_class::FILE_NAME, 'tmpdata').and_return true
+        obj.get_data
+      end
+    end
   end
 end
