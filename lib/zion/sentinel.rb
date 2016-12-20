8bfc07ce08a892e1c2f2c0c3893c5e5becc8bf7e
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2016-12-21 00:20:31 +0100

    Allow sentinel to read to CSV send

--- lib/zion/sentinel.rb
+++ lib/zion/sentinel.rb
@@ -1,4 +1,5 @@
 require 'zip'
+require 'csv'
 
 module Zion
   class Sentinel < Zion::Source
@@ -7,6 +8,20 @@ module Zion
     def get_data
       data = hack_matrix({ source: 'sentinels' })
       File.write(FILE_NAME, data)
+      parse_sentinal
     end
+
+    private
+      def parse_sentinal
+        csv = ''
+        Zip::File.open(FILE_NAME) do |zip_file|
+          csv = zip_file.read('sentinels/routes.csv')
+        end
+        parse_csv(csv.gsub('"', ''))
+      end
+
+      def parse_csv(csv_data)
+        CSV.parse(csv_data, headers: true, quote_char: '"', force_quotes: true)
+      end
   end
 end
--- spec/zion/sentinel_spec.rb
+++ spec/zion/sentinel_spec.rb
@@ -4,6 +4,12 @@ describe Zion::Sentinel do
   describe '#get_data' do
     let(:obj) { described_class.new }
 
+    before do
+      zip_file = double('zip_file')
+      allow(Zip::File).to receive(:open).and_yield zip_file
+      allow(zip_file).to receive(:read).with('sentinels/routes.csv').and_return 'a,b,c\n1,2,3'
+    end
+
     it 'makes a get request to get the CSV data' do
       expect(obj).to receive(:hack_matrix).with({source: 'sentinels'}).and_return ''
       obj.get_data
@@ -16,6 +22,13 @@ describe Zion::Sentinel do
 
       it 'writes the data to the tmp zip file' do
         expect(File).to receive(:write).with(described_class::FILE_NAME, 'tmpdata').and_return true
+        obj.get_data
+      end
+
+      it 'reads the zip file and returns CSV' do
+        zip_file = double('zip_file')
+        expect(Zip::File).to receive(:open).and_yield zip_file
+        expect(zip_file).to receive(:read).with('sentinels/routes.csv').and_return 'a,b,c\n1,2,3'
         obj.get_data
       end
     end
